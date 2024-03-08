5acc688620ef724ef34a224fbef83a68c1e9a175
Sumaiya Choudhury <sumaiyac92@gmail.com>
2024-03-07 22:30:12 -0500

    review class

--- src/com/upskill/java_1/Variables.java
+++ src/com/upskill/java_1/Variables.java
@@ -5,7 +5,7 @@ public class Variables {
 
 	String country = "USA";							//Global or Instance variable - Variables declared in Class level, outside method
 	
-	static String region = "America";				//Static variable - variables belong to class and don't required creating object
+	static String region = "North America";				//Static variable - variables belong to class and don't required creating object
 		
 	public static void main(String[] args) {
 		String city = "New York";					//Local variable - variables declared in methods
